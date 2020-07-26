2b340f5438b820c3879c194d057f3bc967fc66f0
aderp <aderahmanp@gmail.com>
2020-07-26 18:03:41 +0700

    typo

--- routes/digimonRoutes.js
+++ routes/digimonRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const digimonController = require("../contrallers/digimonController");
+const digimonController = require("../controllers/digimonController");
 
 
 router.post("/digimon", digimonController.createDigimon);
